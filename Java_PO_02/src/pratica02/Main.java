530b006dab5136c4b20e7347ad7538241b51927c
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-11-26 11:56:37 -0300

    feature: projetos desenvolvidos em java e pratica02

---
+++ Java_PO_02/src/pratica02/Main.java
@@ -0,0 +1,25 @@
+package pratica02;
+import java.util.Scanner;
+import pratica02.ConverteMoeda;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		ConverteMoeda cm = new ConverteMoeda();
+		
+		System.out.println("Digite a taxa de câmbio (por exemplo, 5.05 para converter de dólares para euros ou reais): ");
+        cm.setTaxaCambio(scanner.nextDouble());
+
+        System.out.println("Digite a quantidade em dólares: ");
+        cm.setQtdDolares(scanner.nextDouble());
+
+        double valorConvertido = ConverteMoeda.calculaConversaoMoeda(cm.getQtdDolares(), cm.getTaxaCambio());
+    
+        System.out.printf("O valor convertido é: %.2f%n", valorConvertido);
+
+	}
+
+}
