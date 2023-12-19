b6d208d768457693177fb671cec7121e3409e577
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-12-18 22:22:02 -0300

    classe calculadora e pedido

---
+++ Pratica04Trilha/src/pratica04/Calculadora.java
@@ -0,0 +1,91 @@
+package pratica04;
+
+import java.util.ArrayList;
+
+public class Calculadora {
+
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("Não é possível dividir por zero.");
+        }
+    }
+
+
+    public static float somar(float a, float b) {
+        return a + b;
+    }
+
+    public static float subtrair(float a, float b) {
+        return a - b;
+    }
+
+    public static float multiplicar(float a, float b) {
+        return a * b;
+    }
+
+    public static float dividir(float a, float b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("Não é possível dividir por zero.");
+        }
+    }
+
+
+    public static int somar(ArrayList<Integer> numeros) {
+        int resultado = 0;
+        for (int num : numeros) {
+            resultado += num;
+        }
+        return resultado;
+    }
+
+    public static int multiplicar(ArrayList<Integer> numeros) {
+        int resultado = 1;
+        for (int num : numeros) {
+            resultado *= num;
+        }
+        return resultado;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Operações com inteiros:");
+        System.out.println("Soma: " + somar(5, 3));
+        System.out.println("Subtração: " + subtrair(5, 3));
+        System.out.println("Multiplicação: " + multiplicar(5, 3));
+        System.out.println("Divisão: " + dividir(5, 3));
+
+        
+        System.out.println("\nOperações com floats:");
+        System.out.println("Soma: " + somar(5.0f, 3.0f));
+        System.out.println("Subtração: " + subtrair(5.0f, 3.0f));
+        System.out.println("Multiplicação: " + multiplicar(5.0f, 3.0f));
+        System.out.println("Divisão: " + dividir(5.0f, 3.0f));
+
+
+        System.out.println("\nOperações com ArrayList de inteiros:");
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(2);
+        numeros.add(4);
+        numeros.add(2);
+        System.out.println("Soma: " + somar(numeros));
+        System.out.println("Multiplicação: " + multiplicar(numeros));
+    }
+}
---
+++ Pratica04Trilha/src/pratica04/Pedido.java
@@ -0,0 +1,94 @@
+package pratica04;
+import java.util.ArrayList;
+
+
+
+class ItemPedido {
+    String nome;
+    double preco;
+
+    public ItemPedido(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
+
+
+public class Pedido {
+	
+	private long numeroPedido;
+	private String cpf;
+	private ArrayList<ItemPedido> itens;
+	
+    public Pedido(int numeroPedido, String cpf) {
+        this.numeroPedido = numeroPedido;
+        this.cpf = cpf;
+        this.itens = new ArrayList<>();
+    }
+    
+
+    public void adicionarItem(String nome, double preco) {
+        itens.add(new ItemPedido(nome, preco));
+    }
+
+
+    public void adicionarItem(ArrayList<ItemPedido> listaItens) {
+        itens.addAll(listaItens);
+    }
+
+
+    public double calcularTotal(double percentualDesconto) {
+        double total = 0;
+        for (ItemPedido item : itens) {
+            total += item.preco;
+        }
+        double desconto = total * (percentualDesconto / 100);
+        return total - desconto;
+    }
+
+    public double calcularTotal(int numeroPrestacoes, double taxaJuros) {
+        double total = 0;
+        for (ItemPedido item : itens) {
+            total += item.preco;
+        }
+        double juros = total * (taxaJuros / 100);
+        return total + juros;
+    }
+
+
+    public void exibirInformacoes() {
+        System.out.println("Número do Pedido: " + numeroPedido);
+        System.out.println("CPF do Cliente: " + cpf);
+        System.out.println("Itens do Pedido:");
+        for (ItemPedido item : itens) {
+            System.out.println(item.nome + ": R$" + item.preco);
+        }
+    }
+	
+	
+	
+	
+	
+
+
+	public static void main(String[] args) {
+		 Pedido pedidoComDesconto = new Pedido(1, "123.456.789-00");
+        pedidoComDesconto.adicionarItem("Produto1", 50.0);
+        pedidoComDesconto.adicionarItem("Produto2", 30.0);
+        pedidoComDesconto.exibirInformacoes();
+        System.out.println("Total com desconto à vista: R$" + pedidoComDesconto.calcularTotal(10.0));
+
+        System.out.println("\n------------------------\n");
+
+        
+        Pedido pedidoAPrazo = new Pedido(2, "987.654.321-00");
+        ArrayList<ItemPedido> listaItens = new ArrayList<>();
+        listaItens.add(new ItemPedido("Produto3", 40.0));
+        listaItens.add(new ItemPedido("Produto4", 25.0));
+        pedidoAPrazo.adicionarItem(listaItens);
+        pedidoAPrazo.exibirInformacoes();
+        System.out.println("Total a prazo: R$" + pedidoAPrazo.calcularTotal(3, 5.0));
+
+	}
+
+}
