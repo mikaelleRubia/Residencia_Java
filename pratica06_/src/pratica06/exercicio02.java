0cb4faf8c9eb6b67d5620e09ca4b86a91c7ef9c8
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2024-01-14 20:47:32 -0300

    feature: bufferdWriter e FileWriter

--- pratica06_/src/pratica06/exercicio02.java
+++ pratica06_/src/pratica06/exercicio02.java
@@ -1,11 +1,48 @@
 package pratica06;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
+
 
 public class exercicio02 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
+//		String[] lines = new String [] {"bom dia", " boa tarde", "boa noite"};
+		List<String> lines02 = new ArrayList<>();
+		char resposta;
+		
+		
+		String file = ("/home/mikaelle/Documents/Residencia/cloneRepositorio/Residencia_Java/pratica06_/inserirDados.txt");
+		Scanner sc = new Scanner(System.in);
 
+
+		
+		 try(BufferedWriter br = new BufferedWriter(new FileWriter(file))) {
+			 do {
+				 
+				System.out.println("Deseja a string para ser adicionado no arquivo");
+                String input = sc.next();
+                lines02.add(input);
+
+				 
+				System.out.println("Deseja inserir outra string (S/N)");
+				resposta = Character.toUpperCase(sc.next().charAt(0));
+			 
+			 }while(resposta =='S');
+			 for(String line : lines02) {
+				 br.write(line);
+				 br.newLine(); 
+			 }
+		 }
+		 catch(IOException e) {
+			 System.out.println("error: "+ e.getMessage());
+		 } 
 	}
 
 }
