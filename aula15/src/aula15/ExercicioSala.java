9562cf45a47c8d83808c7650bcd2b1bd02b5e7e0
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2024-01-15 12:15:18 -0300

    feature: munipulação de arquivo txt, criptografia

--- aula15/src/aula15/ExercicioSala.java
+++ aula15/src/aula15/ExercicioSala.java
@@ -1,7 +1,9 @@
 package aula15;
 
 import java.io.IOException;
+
 import aula15.Cripto;
+import aula15.CriptoText;
 
 public class ExercicioSala {
 	 public static void main(String[] args) {
@@ -12,8 +14,9 @@ public class ExercicioSala {
 		 System.out.println("Entrada :" + nome1);
 		 System.out.println("Saida :"+ nome2);
 		 try {
-	            Cripto.process(nome1, nome2, senha);
-	            Cripto.process(nome2, "testes"+nome1, senha);
+//	            Cripto.process(nome1, nome2, senha);
+//	            Cripto.process(nome2, "testes"+nome1, senha);
+	            CriptoText.process02(nome1, nome2, senha);
 	        } catch (Exception e) {
 	            System.out.println(e.getMessage());
 	            e.printStackTrace();
