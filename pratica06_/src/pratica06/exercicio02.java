d3702da4453c69b444796fd05162cebeaf27044f
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2024-01-14 23:29:54 -0300

    feature: exercicio 04

--- pratica06_/src/pratica06/exercicio02.java
+++ pratica06_/src/pratica06/exercicio02.java
@@ -3,50 +3,37 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.List;
-
-
+import java.util.Scanner;
 
 public class exercicio02 {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		
-//		String[] lines = new String [] {"bom dia", " boa tarde", "boa noite"};
-		List<String> lines02 = new ArrayList<>();
-		char resposta;
-		
-		
-		String file = ("/home/mikaelle/Documents/Residencia/cloneRepositorio/Residencia_Java/pratica06_/inserirDados.txt");
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        List<String> lines02 = new ArrayList<>();
+        String resposta;
 
+        String file = ("/home/mikaelle/Documents/Residencia/cloneRepositorio/Residencia_Java/pratica06_/inserirDados.txt");
+        Scanner sc = new Scanner(System.in);
 
-		
-		 try(BufferedWriter br = new BufferedWriter(new FileWriter(file, true))) {
-			 do {
-				 
-				System.out.println("Deseja a string para ser adicionado no arquivo");
-                String input = sc.next();
+        try (BufferedWriter br = new BufferedWriter(new FileWriter(file, true))) {
+            do {
+                System.out.println("Deseja a frase para ser adicionada no arquivo");
+                String input = sc.nextLine();
                 lines02.add(input);
 
-				 
-				System.out.println("Deseja inserir outra string (S/N)");
-				resposta = Character.toUpperCase(sc.next().charAt(0));
-			 
-			 }while(resposta =='S');
-			 
-			 for(String line : lines02) {
-				 br.write(line);
-				 br.newLine(); 
-			 }
-			 System.out.println("Conteúdo adicionado no arquivo com sucesso!");
-		 }
-		 catch(IOException e) {
-			 System.out.println("error: "+ e.getMessage());
-		 } 
-		 sc.close();
-	}
-	
+                System.out.println("Deseja inserir outra frase (S/N)");
+                resposta = sc.nextLine().toUpperCase();
 
+            } while (resposta.equals("S"));
+
+            for (String line : lines02) {
+                br.write(line);
+                br.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            sc.close();
+        }
+    }
 }
